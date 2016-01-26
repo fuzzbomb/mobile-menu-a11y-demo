08aadfed6b3fff63a4993bde56b1f370d34130f7
Andrew Macpherson <hopscotch23@gmail.com>
2016-01-26 22:03:06 +0000

    Add open/close behaviour to main-menu.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 /**
  * @file
+ * Set-up main menu as a mobile-style burger menu.
  */
 
 (function ($) {
   $(document).ready(function() {
+
+    // Initialize.
     $('#main-menu nav').hide();
+
+    // Add the open/close behaviour.
+    $('#main-menu-button').click(function(event) {
+      $('#main-menu nav').toggle();
+    });
   });
 })(jQuery);
