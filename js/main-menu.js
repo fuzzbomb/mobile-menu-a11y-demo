f6f639f22a78ad66c215fbc5067250a318aa0912
Andrew Macpherson <hopscotch23@gmail.com>
2016-01-26 22:16:23 +0000

    Move main-menu behaviour to it's own JS file.

---
+++ js/main-menu.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Set-up main menu as a mobile-style burger menu.
+ */
+
+(function ($) {
+  $(document).ready(function() {
+
+    // Initialize.
+    var menuDisplayed = false;
+    $('#main-menu nav').hide();
+
+    // Add the open/close behaviour.
+    $('#main-menu-button').click(function(event) {
+      if (menuDisplayed === true) {
+        $('#main-menu nav').hide();
+        menuDisplayed = false;
+      }
+      else {
+        $('#main-menu nav').show();
+        menuDisplayed = true;
+      }
+    });
+  });
+})(jQuery);
