ad3fa57ac9144eea0ad73062669aaf7df567c27a
Andrew Macpherson <hopscotch23@gmail.com>
2016-01-26 23:29:42 +0000

    Added aria-controls property and aria-expanded state to main-menu button.

--- js/main-menu.js
+++ js/main-menu.js
@@ -12,6 +12,8 @@
 
     // Add the open/close behaviour.
     $('#main-menu-button')
+      .attr('aria-controls', 'main-menu-nav')
+      .attr('aria-expanded', false)
       .keypress( function(event) {
         // Only respond to enter or spacebar keys
         if (event.which == 13 || event.which == 32) {
@@ -22,10 +24,12 @@
       })
       .click(function(event) {
         if (menuDisplayed === true) {
+          $(this).attr('aria-expanded', false)
           $('#main-menu nav').hide();
           menuDisplayed = false;
         }
         else {
+          $(this).attr('aria-expanded', true)
           $('#main-menu nav').show();
           menuDisplayed = true;
         }
