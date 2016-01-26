323264e646f67a9b36ac77f6caaeb3b33bdc3aa3
Andrew Macpherson <hopscotch23@gmail.com>
2016-01-26 22:09:15 +0000

    Refactor main-menu open/close behaviour to use show + hide methods instead of toggle.
    Preparation for toggling other properties later on.
    This is more verbose, but more explicit for demonstration purposes.

--- js/main.js
+++ js/main.js
@@ -7,11 +7,19 @@
   $(document).ready(function() {
 
     // Initialize.
+    var menuDisplayed = false;
     $('#main-menu nav').hide();
 
     // Add the open/close behaviour.
     $('#main-menu-button').click(function(event) {
-      $('#main-menu nav').toggle();
+      if (menuDisplayed === true) {
+        $('#main-menu nav').hide();
+        menuDisplayed = false;
+      }
+      else {
+        $('#main-menu nav').show();
+        menuDisplayed = true;
+      }
     });
   });
 })(jQuery);
