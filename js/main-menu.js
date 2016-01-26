8957d86ec64e41db41e7af15e17557b8fc6f1bb5
Andrew Macpherson <hopscotch23@gmail.com>
2016-01-26 23:57:32 +0000

    BUGFIX - Removed redundant keypress detection JS logic.
    This had actually become a liability.
    Once the button had become a <button>, it had TWO keypress handlers - one
    from the browser itself, and one from our custom JS. Since both trigger a click,
    it had the undesired effect of double-toggling the menu, making it innaccessible to keyboard users.

--- js/main-menu.js
+++ js/main-menu.js
@@ -14,14 +14,6 @@
     $('#main-menu-button')
       .attr('aria-controls', 'main-menu-nav')
       .attr('aria-expanded', false)
-      .keypress( function(event) {
-        // Only respond to enter or spacebar keys
-        if (event.which == 13 || event.which == 32) {
-          // Spacebar usually triggers page scrolling.
-          event.preventDefault();
-          $(this).trigger('click');
-        }
-      })
       .click(function(event) {
         if (menuDisplayed === true) {
           $(this).attr('aria-expanded', false)
