3b8622cafd3541c9770d5ca775e19540e3322090
Andrew Macpherson <hopscotch23@gmail.com>
2016-01-26 23:05:10 +0000

    Added key detection to main-menu button.
    Only respond to enter or spacebar.

--- js/main-menu.js
+++ js/main-menu.js
@@ -13,7 +13,12 @@
     // Add the open/close behaviour.
     $('#main-menu-button')
       .keypress( function(event) {
-        $(this).trigger('click');
+        // Only respond to enter or spacebar keys
+        if (event.which == 13 || event.which == 32) {
+          // Spacebar usually triggers page scrolling.
+          event.preventDefault();
+          $(this).trigger('click');
+        }
       })
       .click(function(event) {
         if (menuDisplayed === true) {
